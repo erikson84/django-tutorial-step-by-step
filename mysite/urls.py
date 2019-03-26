a03c4b78a480d23de9b4c6ff49a3dc91ba1a45b1
Erikson Kaszubowski <erikson.k@ufsc.br>
2019-03-25 21:02:41 -0300

    More tests!

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('', include('polls.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
